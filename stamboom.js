ae873e38594d875382ff03516c0a899f410322a5
JaschaBerkhout <76992119+JaschaBerkhout@users.noreply.github.com>
2022-12-20 16:32:04 +0100

    class familie, toevoegen aan weergave

--- stamboom.js
+++ stamboom.js
@@ -1,4 +1,59 @@
-let familie = []
+class Familie {
+  constructor(){
+    this.personen = []
+  }
+
+  addPersoon(persoon){
+    persoon.id = this.aantalPersonen() + 1
+    this.personen.push(persoon);
+    this.sortAge()
+    updateSamenvatting()
+    addToStorage()
+    updatenVanStamboomWeergave()
+  }
+
+  ageTotal(){
+    let ageTotal = 0
+    
+    this.personen.forEach(persoon => ageTotal += persoon.bepaalLeeftijd())
+        
+    return ageTotal;  
+  }
+
+  aantalPersonen(){
+    return this.personen.length
+  }
+  
+  ageAverage(){
+	  return this.ageTotal() / this.aantalPersonen(); 
+  }
+
+  sortAge(){
+    this.personen = this.personen.sort((a, b) => b.bepaalLeeftijd() - a.bepaalLeeftijd())
+  }
+  
+  oudste(){
+    return this.personen[0]
+  }
+  
+  jongste(){
+    return this.personen[this.aantalPersonen() - 1]
+  }
+  
+  partOfFam(start, end){
+    let partOfFam = []
+    
+    if(end > this.aantalPersonen() ){
+        end = this.aantalPersonen()
+    }
+    
+    for (let i=start; i < end; i++){
+        partOfFam.push(this.personen[i])
+    }
+    
+    return partOfFam
+  }
+}
 
 class Persoon {
   constructor(fname, lname, gender, birthday){
@@ -22,125 +77,121 @@ class Persoon {
   name(){
     return this.fname + ' ' + this.lname
   }
+  mooiTekstje() {
+    return "<div class='"+ (this.gender === 'm' ? 'man' : 'vrouw')+ " persoon'>"+this.name() +
+     ' <br> '+ this.bepaalLeeftijd()+
+     ' jaar' +
+     ' <br>*' +
+     this.mooieVerjaardag() +
+     '</div>';
+  }
+  mooieVerjaardag() {
+    const geboortedatum = new Date(this.birthday)
+    return geboortedatum.toLocaleDateString('nl-nl');
+  }
 };
-
-// const jascha = new Persoon('Jascha','Berkhout','v','1995-02-16');
-
-// const dunja = new Persoon('Dunja','Berkhout','v','1997-05-26') 
-
-// const zoey = new Persoon('Zoey','Berkhout','v','2002-08-27') 
-
-// const mama = new Persoon('Marry','van de Ruit','v','1969-12-14')
-
-function addToFamilie(person){
-  familie.push(person);
-  sortAge()
-  updateSamenvatting()
-  addToStorage()
-};
-  
-// addToFamilie(jascha);
-// addToFamilie(dunja);
-// addToFamilie(zoey);
-// addToFamilie(mama);
-
-function aantalPersonenInStamboom(){
-  return familie.length
-};
-
-
-function ageTotal2(){
-  let ageTotal = 0
-  
-  familie.forEach(familieLid => ageTotal += familieLid.bepaalLeeftijd())
-  
-  
-  return ageTotal;  
-};
-
-
-function ageAverage(){
-	
-  return ageTotal2() / familie.length; 
-};
-
-// selectie uit familie halen met slice functie uitgeschreven
-function partOfFam(start, end){
-    let partOfFam = []
-    
-    if(end > familie.length ){
-        end = familie.length
-    }
-    
-    for (let i=start; i < end; i++){
-        partOfFam.push(familie[i])
-    }
-    
-    return partOfFam
-};
-// console.log(partOfFam(1,3));
-
-
-function sortAge(){
-  familie = familie.sort((a, b) => b.age - a.age)
-};
-
-
-function oudste(){
-  return familie[0]
-};
-
-
-function jongste(){
-  return familie[familie.length - 1]
-};
-
 
 function updateSamenvatting() {
   const samenvatting = document.getElementById('samenvatting')
       
   console.log(samenvatting)
 
-  let achternaam = ''
-  let aantalPersonen = aantalPersonenInStamboom()
+  let voornaam = DeFamilie.oudste().fname
+  let achternaam = DeFamilie.oudste().lname
+  const aantalPersonen = DeFamilie.aantalPersonen()
 
   let samenvattingTekst = () => {
-  if (aantalPersonen === 1 ){
-      return `De familie ${achternaam} bevat ${aantalPersonen} persoon.`
-  } 
-  else {
-      return `De familie ${achternaam} bevat ${aantalPersonen} personen.`
+    if (aantalPersonen === 1 ){
+      return `De familie ${achternaam} bevat nu ${aantalPersonen} persoon.`
+    } 
+    else {
+      return `De familie ${achternaam} bevat nu ${aantalPersonen} personen.`
+    }
   }
-  }
+  
+  samenvatting.innerHTML = samenvattingTekst() + ' De gemiddelde leeftijd van de familie is ' + DeFamilie.ageAverage().toFixed(2) + ' jaar.'
+}
 
-  samenvatting.innerHTML = samenvattingTekst() + ' De gemiddelde leeftijd van de familie is ' + ageAverage().toFixed(2) + ' jaar.'
+function meldingWeghalen(){  
+  setTimeout(() => document.getElementById('melding').innerHTML = '', 3000);
+} 
+
+// generieke/algemene melding
+function algemeneMelding(tekst){
+  const meldingElement = document.getElementById('melding')
+  meldingElement.innerHTML = tekst
+  meldingWeghalen()
+}
+
+
+function meldingNieuwPersoon(persoon) {
+ algemeneMelding(`${persoon.fname} is toegevoegd aan de familie ${persoon.lname}.`);
+  
 }
 
 const name = document.getElementById('fname').value + ' ' + document.getElementById('lname').value
 
-function formInvullen(){
-  const fname = document.getElementById('fname').value
-  const lname = document.getElementById('lname').value
-  const birthday = document.getElementById('bday').value
-  if(document.querySelector('input[name="gender"]:checked') === null){
-    return 
+function nieuwPersoonToevoegen(){
+  
+  const fnameElement = document.getElementById('fname');
+  const lnameElement = document.getElementById('lname');
+  const genderElement = document.querySelector('input[name="gender"]:checked');
+  const bdayElement = document.getElementById('bday');
+  
+  const fname = fnameElement.value;
+  const lname = lnameElement.value;
+  const birthday = bdayElement.value;
+  
+  if(genderElement === null || fname === '' || lname === '' || birthday === ''){
+    algemeneMelding('Leeg invulveld.')
+    return;
   }
-  const gender = document.querySelector('input[name="gender"]:checked').value
+  
+  const gender = genderElement.value;
+  const persoon = new Persoon(fname,lname,gender,birthday)
+  DeFamilie.addPersoon(persoon);
+  
+  fnameElement.value = '';
+  lnameElement.value = '';
+  bdayElement.value = '';
+  genderElement.checked = false;
 
-  addToFamilie(new Persoon(fname,lname,gender,birthday))
+  meldingNieuwPersoon(persoon);
+
 };
 
-
 function addToStorage(){
-  window.localStorage.setItem('familie',JSON.stringify(familie))
+  window.localStorage.setItem('personen',JSON.stringify(DeFamilie.personen))
 };
 
 function getFromStorage(){
-  return JSON.parse(window.localStorage.getItem('familie'))
+  return JSON.parse(window.localStorage.getItem('personen'))
 };
 
-const readdToFam = () => {
-  getFromStorage().forEach(persoon => addToFamilie(
-    new Persoon(persoon.fname, persoon.lname, persoon.gender,persoon.birthday)))
+const bouwFamilieVanStorage = () => {
+  
+  const personenUitStorage = getFromStorage()
+  if(personenUitStorage === null) {
+    return;
+  }
+  
+  personenUitStorage.forEach(persoon => DeFamilie.addPersoon(
+    new Persoon(persoon.fname, persoon.lname, persoon.gender,persoon.birthday))
+  )
 };
-readdToFam();
+
+// begin van de stamboom website
+const DeFamilie = new Familie();
+bouwFamilieVanStorage();
+updatenVanStamboomWeergave();
+
+function updatenVanStamboomWeergave(){
+  const personenElement = document.getElementById('personen')
+  personenElement.innerHTML = allesVanDeFamilie();
+}
+
+function allesVanDeFamilie() {
+  let result = '';
+  DeFamilie.personen.forEach(persoon => result += persoon.mooiTekstje())
+  return result;
+}
